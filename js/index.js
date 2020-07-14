582aba43ada16cd632a281100f5b935490c52e60
elbie-em <emoonga1993@gmail.com>
2020-07-14 20:40:27 +0200

    add lodash to project

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import _ from 'lodash';
 
 console.log('Hello From JavaScript');
 console.log(moment().startOf('day').fromNow());
@@ -7,3 +8,16 @@ console.log('Using Babel');
 
 var name = "Elbie", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
+
+function component() {
+  const element = document.createElement('div');
+
+  
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
+}
+
+let container = document.getElementById('cont');
+
+container.appendChild(component());
